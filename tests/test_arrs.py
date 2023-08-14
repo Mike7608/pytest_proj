f73a75c87842605888929106db76612f3f1c58fb
Mike Kushner <mike760807@gmail.com>
2023-08-14 19:41:41 +0300

    add new tests

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -2,10 +2,16 @@ from utils import arrs
 
 
 def test_get():
-    assert arrs.get([1, 2, 3], 1, "test") == 3
-    assert arrs.get([], 0, "test") == "test"
+    assert arrs.get([1, 2, 3], 1, "test") == 2
+
+
+def test_get_default():
+    assert arrs.get([1,2,3], -1)  == None
 
 
 def test_slice():
     assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
     assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
+    assert arrs.my_slice([]) == []
+    assert arrs.my_slice([1,2,3], -1) == [3]
+    assert arrs.my_slice([1,2,3],-5,) == [1,2,3]
